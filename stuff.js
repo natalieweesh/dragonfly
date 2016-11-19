83c8a15ecb5c551388b472cee8d832a38415d8c5
Natalie Wee <natalieweesh@gmail.com>
2016-11-19 14:21:46 -0500

    finish skeleton

---
+++ stuff.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+  document.getElementsByTagName("nav")[0].classList.toggle("open");
+});
+document.getElementById("close-hamburger").addEventListener("click", function() {
+  document.getElementsByTagName("nav")[0].classList.toggle("open");
+});
