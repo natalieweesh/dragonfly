f19cba52ba7133d04ad07e8c407b3ae1fe03a827
Natalie Wee <natalieweesh@gmail.com>
2016-11-19 15:03:44 -0500

    freeze scrolling when nav menu is open

--- stuff.js
+++ stuff.js
@@ -1,7 +1,5 @@
 document.getElementById("hamburger").addEventListener("click", function() {
   document.getElementsByTagName("nav")[0].classList.toggle("open");
   document.getElementById("hamburger").classList.toggle("open");
-});
-document.getElementById("close-hamburger").addEventListener("click", function() {
-  document.getElementsByTagName("nav")[0].classList.toggle("open");
+  document.getElementsByTagName("body")[0].classList.toggle("nav-open");
 });
