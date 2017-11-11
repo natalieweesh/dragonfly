c2e2d2ce1be76c6c18891fe33eeeade857326144
natalie wee <natalieweesh@gmail.com>
2017-11-11 08:36:57 -0500

    remove comments from compass generated css

--- config.rb
+++ config.rb
@@ -17,9 +17,17 @@ javascripts_dir = "javascripts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+css_dir = "stylesheets" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :expanded # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
